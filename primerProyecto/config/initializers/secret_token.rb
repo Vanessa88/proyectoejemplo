88eea4d8409381a574c33a1f6498e38e3e051f53
Vanessa <neis88@hotmail.com>
2011-11-30 00:12:19 -0500

    rails new

---
+++ primerProyecto/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrimerProyecto::Application.config.secret_token = 'c1f3985ed93b20af80d199181d15d4c0f90765827d207e7d6c0d82c8b177b2d43f15fd6b0b6dce56d710b98ac24a570215d73e7742fac083fe77b4dd330e3924'
